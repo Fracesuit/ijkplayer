a4c4e70d85f6b056236e93dd99767e897aa3c513
zhiren.zhang <915924056@qq.com>
2019-07-12 14:46:26 +0800

    111111

--- android/ijkplayer/ijkplayer-armv7a/src/main/obj/local/armeabi-v7a/objs/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/zzr/ijkplayer-android/android/ijkplayer/ijkplayer-armv7a/src/main/obj/local/armeabi-v7a/objs/cpufeatures/cpu-features.o: \
- /home/zzr/Android/Sdk/ndk-bundle/sources/android/cpufeatures/cpu-features.c \
- /home/zzr/Android/Sdk/ndk-bundle/sources/android/cpufeatures/cpu-features.h
-
-/home/zzr/Android/Sdk/ndk-bundle/sources/android/cpufeatures/cpu-features.h:
